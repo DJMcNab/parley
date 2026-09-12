aa0240b7650296b3b4d51e394bb90bb8facf271b
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2026-09-12 13:46:42 +1000

    Avoid re-exporting `AnalysisDataSources`

--- parley/src/tests/test_emoji.rs
+++ parley/src/tests/test_emoji.rs
@@ -3,11 +3,9 @@
 
 use alloc::vec::Vec;
 use core::char;
+use parley_engine::AnalysisDataSources;
 
-use crate::{
-    AnalysisDataSources,
-    emoji::{EmojiDFA, EmojiPresentationStyle, EmojiSegmentationCategory},
-};
+use crate::emoji::{EmojiDFA, EmojiPresentationStyle, EmojiSegmentationCategory};
 
 struct TestEntity<'a> {
     sequence: &'a [u32],
